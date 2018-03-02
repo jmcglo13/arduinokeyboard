fee6a02c46327903575638975f8196c6964e66a3
Josiah McGlothin <jmcglo13@gmail.com>
2018-03-02 00:20:36 -0800

    Removed some comments, testing my commits!

--- Keyboard/Keyboard.ino
+++ Keyboard/Keyboard.ino
@@ -1,33 +1,11 @@
-/*
-  ESP8266 Blink by Simon Peter
-  Blink the blue LED on the ESP-01 module
-  This example code is in the public domain
-
-  The blue LED on the ESP-01 module is connected to GPIO1
-  (which is also the TXD pin; so we cannot use Serial.print() at the same time)
-
-  Note that this sketch uses LED_BUILTIN to find the pin with the internal LED
-*/
-
 #include <Keyboard.h>
+
 String username = "admin";
-//String password = "1111";
-String password = "19941515112231";
+String password = "19941515112231"; // not a real password, lol.
 
 //ADDING A COMMENT!
 
 void setup() {
-  //  SET UP INITIAL BUFFER for PASSWORD CHARS
-  //  delay(3000);
-  //  char mybuffer[password.length()];
-  //  password.toCharArray(mybuffer, password.length());
-
-  //  for (byte i = 0; i <= password.length()+1; i++) {
-  //    Serial.print(mybuffer[i]);
-  //    delay(50);
-  //  }
-
-
   pinMode(17, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
 
   //  delay(200);
@@ -46,17 +24,6 @@ void setup() {
 
   delay(3000);
   Keyboard.begin();
-  //  Keyboard.print("");
-  //  delay(500);
-  //  Keyboard.write(KEY_RETURN);
-
-//  for (byte i = 0; i < username.length(); i++) {
-//    Keyboard.print(username[i]);
-//    delay(200);
-//  }
-//  Keyboard.write(KEY_RETURN);
-//  delay(3000);
-
   for (byte i = 0; i < password.length(); i++) {
     Keyboard.print(password[i]);
     delay(200);
